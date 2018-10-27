afb0ea4a44ee8c4a728e3a1d709a5a794ef867c4
PC\Administrator <15239928381@163.com>
2018-10-27 23:37:01 +0800

    no message

--- C/passwordInput.c
+++ C/passwordInput.c
@@ -61,3 +61,37 @@ User ui_print_login_dialog() {
 
     return user;
 }
+
+// 安全的输入密码
+void ui_password_input_safely(char *password) {
+
+    char c;
+    for (int i = 0;;) {
+
+        c = getch();
+
+        // 如果是删除键
+        if ((int) (c) == 8) {
+
+            if (i > 0) {
+
+                i = i - 1;
+                printf("\b \b");
+            }
+        }
+            // 如果是回车键
+        else if (c == '\n' || c == '\r') {
+
+            password[i] = '\0';
+            putchar('\n');
+            break;
+        }
+            // 不是删除键也不是回车键，则输出星号
+        else {
+
+            password[i] = c;
+            putch('*');
+            ++i;
+        }
+    }
+}
