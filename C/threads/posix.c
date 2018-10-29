cd32b039bf883357d90b27cd0833ca113fbe977d
PC\Administrator <15239928381@163.com>
2018-10-29 16:36:47 +0800

    no message

--- C/threads/posix.c
+++ C/threads/posix.c
@@ -2,57 +2,61 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
-void print_message_function (void *ptr);
+int print_message_function (void *ptr);
 
 int main()
 {
-    int tmp1, tmp2;
-    void *retval;
+
+    // 被等待线程的返回值
+    void *retval; // retval是无类型的指针，可以被任意指针赋值
+
+    // 定义2个线程
     pthread_t thread1, thread2;
+
     char *message1 = "thread1";
     char *message2 = "thread2";
 
-    int ret_thrd1, ret_thrd2;
-
-    ret_thrd1 = pthread_create(&thread1, NULL, (void *)&print_message_function, (void *) message1);
-    ret_thrd2 = pthread_create(&thread2, NULL, (void *)&print_message_function, (void *) message2);
+    // 线程1和线程2的创建状态
+    int thread1_create_status = pthread_create(&thread1, NULL, (void *)&print_message_function, (void *) message1);
+    int thread2_create_status = pthread_create(&thread2, NULL, (void *)&print_message_function, (void *) message2);
 
     // 线程创建成功，返回0,失败返回失败号
-    if (ret_thrd1 != 0) {
+    if (thread1_create_status != 0) {
+
         printf("线程1创建失败\n");
-    } else {
-        printf("线程1创建成功\n");
     }
-
-    if (ret_thrd2 != 0) {
+    if (thread2_create_status != 0) {
         printf("线程2创建失败\n");
-    } else {
-        printf("线程2创建成功\n");
     }
 
-    //同样，pthread_join的返回值成功为0
-    tmp1 = pthread_join(thread1, &retval);
-    printf("thread1 return value(retval) is %d\n", (int)retval);
-    printf("thread1 return value(tmp) is %d\n", tmp1);
-    if (tmp1 != 0) {
-        printf("cannot join with thread1\n");
+
+
+    // 主线程以阻塞的方式等待线程thread1的结束
+    if(0 != pthread_join(thread1, &retval)){
+
+        printf("the main thread pthread_join thread1 with a error\n");
     }
+    printf("thread1 return value(retval) is %d\n", (int)retval);
     printf("thread1 end\n");
 
-    tmp2 = pthread_join(thread1, &retval);
-    printf("thread2 return value(retval) is %d\n", (int)retval);
-    printf("thread2 return value(tmp) is %d\n", tmp1);
-    if (tmp2 != 0) {
-        printf("cannot join with thread2\n");
+    // 主线程以阻塞的方式等待线程thread2的结束
+    if(0 != pthread_join(thread2, &retval)){
+
+        printf("the main thread pthread_join thread2 with a error\n");
     }
+    printf("thread2 return value(retval) is %d\n", (int)retval);
     printf("thread2 end\n");
 
+    return 0;
 }
 
-void print_message_function( void *ptr ) {
+int print_message_function( void *ptr ) {
     int i = 0;
-    for (i; i<5; i++) {
+
+    for (i; i<10; i++) {
+
         printf("%s:%d\n", (char *)ptr, i);
     }
+
+    return 98;
 }
