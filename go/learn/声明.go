97336230e349a6dbaacdb032995444c4c924402e
PC\Administrator <15239928381@163.com>
2018-11-03 20:56:11 +0800

    no message

---
+++ go/learn/声明.go
@@ -0,0 +1,30 @@
+// 常量定义
+const NAME string = "fd"
+
+// 全局变量的声明与赋值
+var a string = "gfd"
+
+// 一般类型的声明
+type count int;
+
+// 结构的声明
+type Car struct{
+
+}
+
+// 接口的声明
+type ICar interface{
+
+
+}
+
+// 函数定义
+func run(){
+
+}
+
+// main 主函数
+func main(){
+
+}
+
