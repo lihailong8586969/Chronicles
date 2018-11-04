5ef7ecb24a1cb43fe49cc06c5bf04bf37a89e114
PC\Administrator <15239928381@163.com>
2018-11-04 08:28:26 +0800

    no message

---
+++ go/learn/struct.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Man struct{
+
+	name string;
+	age int;
+}
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+
+func main() {
+
+	// 初始化字段不一定要全部指定，比如下面也是可以的，name默认取长度为0的空字符串
+	man := Man{"jayChou"}
+
+	// 全部字段都初始化
+	man := Man{"jayChou",20}
+	fmt.Println(man.name)
+
+
+	v := Vertex{1, 2}	
+	v.X = 4
+	p := &v
+	p.X = 1e9	
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+
+
+
+	fmt.Println("Hello, 世界")
+}
