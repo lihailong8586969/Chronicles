0c38405c71bacfbfe4d94862ea80ff142e7b2355
PC\Administrator <15239928381@163.com>
2018-11-05 10:25:05 +0800

    no message

--- Nodejs/guide.js
+++ Nodejs/guide.js
@@ -1,7 +1,6 @@
 var http = require("http");
 
-var http = require('http');
-
+// 创建服务器
 http.createServer(function (request, response) {
 
     // 发送 HTTP 头部 
@@ -15,3 +14,51 @@ http.createServer(function (request, response) {
 
 // 终端打印如下信息
 console.log('Server running at http://127.0.0.1:8888/');
+
+
+// 同步读取文件( 阻塞的 )
+var fs = require("fs");
+
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("程序执行结束!");
+
+
+// 异步读取文件( 非阻塞 )
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("程序执行结束!");
+
+
+// 引入 events 模块
+var events = require('events');
+
+// 创建 eventEmitter 对象 ( Emitter 发出者, 发射者 )
+var eventEmitter = new events.EventEmitter();
+
+// 创建事件处理程序
+var connectHandler = function connected() {
+   console.log('连接成功。');
+  
+   // 触发 data_received 事件 
+   eventEmitter.emit('data_received');
+}
+
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', connectHandler);
+ 
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function(){
+   console.log('数据接收成功。');
+});
+
+// 触发 connection 事件 
+eventEmitter.emit('connection');
+
+console.log("程序执行完毕。");
