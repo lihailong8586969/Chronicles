0fa0a97341657c68f651b215a539709b1831ab00
PC\Administrator <15239928381@163.com>
2018-11-06 08:28:10 +0800

    no message

--- go/learn/流程控制/switch.go
+++ go/learn/流程控制/switch.go
@@ -16,15 +16,48 @@ import (
 )
 
 func main() {
+	
 	fmt.Print("Go runs on ")
+
+	// 同 for 一样， if 语句可以在条件表达式前执行一个简单的语句。 
 	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
-	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.", os)
+		
+		// 不用加 break
+		// switch 的 case 语句从上到下顺次执行，直到匹配成功时停止。
+		//（例如，
+		// switch i {
+		//	case 0:
+		//	case f():
+		// }
+		// 在 i==0 时 f 不会被调用。）
+		case "darwin":
+		
+			fmt.Println("OS X.")
+		
+		case "linux":
+		
+			fmt.Println("Linux.")
+		
+		default:
+			// freebsd, openbsd,
+			// plan9, windows...
+			fmt.Printf("%s.", os)
 	}
+
+
+	// 没有条件的 switch, 没有条件的 switch 同 switch true 一样。 
+	// 这种形式能将一长串 if-then-else 写得更加清晰。
+	// 相当于 if (else if) (else if) (else if) else
+	i:=10;
+	
+	switch {
+	case i < 12:
+		fmt.Println("比12小")
+	case i < 17:
+		fmt.Println("比17小")
+	default:
+		fmt.Println("Good evening.")
+	}
+	// 输出 : 比12小
+
 }
