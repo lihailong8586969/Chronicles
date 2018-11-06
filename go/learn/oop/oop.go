8dd9b846db0174ca3614b34a81a157d41f3e9acd
PC\Administrator <15239928381@163.com>
2018-11-06 09:01:45 +0800

    no message

--- go/learn/oop/oop.go
+++ go/learn/oop/oop.go
@@ -8,12 +8,19 @@ type Man struct{
 	age int;
 }
 
+// Go 没有类。不过你可以为结构体类型定义方法。
+// 方法就是一类带特殊的 接收者 参数的函数。
+// 方法接收者在它自己的参数列表内，位于 func 关键字和方法名之间。
+// 方法只是个带接收者参数的函数。
+
 // 对数据的修改只在方法中才生效
 func (man Man) introduce(){
 
 	fmt.Println("Hello My name is ",man.name, ", i am " , man.age , " years old" );
 }
 
+
+// 指针接收者的方法可以修改接收者指向的值（就像 Scale 在这做的）。由于方法经常需要修改它的接收者，指针接收者比值接收者更常用。
 // 对数据的修改会全局生效
 func (man *Man) introduce2(){
 
