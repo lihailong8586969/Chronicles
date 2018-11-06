539ce0441622840b154d694f1a3d76f28232acbc
PC\Administrator <15239928381@163.com>
2018-11-06 22:03:42 +0800

    no message

---
+++ go/learn/string-byte.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+   
+	b := []byte("fds")
+	fmt.Println(b)
+	fmt.Println(string(b))
+	// [102 100 115]
+	// fds
+
+
+	// w 应该是 http 包里的
+	w.write([]byte("Hello World"))
+}
