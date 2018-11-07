c792e392e6012a4cb19010cf1f1b6a09a9589a82
PC\Administrator <15239928381@163.com>
2018-11-07 23:07:09 +0800

    no message

--- React/App/src/scenes/News/index.js
+++ React/App/src/scenes/News/index.js
@@ -1,74 +1,31 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import React, {Component} from 'react';
+import {findDOMNode} from 'react-dom';
+import AppLeft from '../_common/containers/AppLeft/index'
+import AppBody from '../_common/containers/APPBody/index'
+import AppHead from "../_common/containers/AppHead/index";
 
-/* eslint-disable global-require */
+import { Row, Col } from 'antd';
 
-// The top-level (parent) route
-const routes = {
-    path: '',
+class App extends Component{
 
-    // Keep in mind, routes are evaluated in order
-    children: [
-        {
-            path: '',
-            load: () => import(/* webpackChunkName: 'home' */ './home'),
-        },
-        {
-            path: '/contact',
-            load: () => import(/* webpackChunkName: 'contact' */ './contact'),
-        },
-        {
-            path: '/login',
-            load: () => import(/* webpackChunkName: 'login' */ './login'),
-        },
-        {
-            path: '/register',
-            load: () => import(/* webpackChunkName: 'register' */ './register'),
-        },
-        {
-            path: '/about',
-            load: () => import(/* webpackChunkName: 'about' */ './about'),
-        },
-        {
-            path: '/privacy',
-            load: () => import(/* webpackChunkName: 'privacy' */ './privacy'),
-        },
-        {
-            path: '/admin',
-            load: () => import(/* webpackChunkName: 'admin' */ './admin'),
-        },
+    constructor(props) {
 
-        // Wildcard routes, e.g. { path: '(.*)', ... } (must go last)
-        {
-            path: '(.*)',
-            load: () => import(/* webpackChunkName: 'not-found' */ './not-found'),
-        },
-    ],
+        super(props);
+        this.state = {date:new Date()};
+    }
 
-    async action({ next }) {
-        // Execute each child route until one of them return the result
-        const route = await next();
+    render(){
 
-        // Provide default values for title, description etc.
-        route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
-        route.description = route.description || '';
+        return (
 
-        return route;
-    },
-};
+            <div id="Route-to-News">
+                <p>News</p>
+            </div>
 
-// The error page is available by permanent url for development mode
-if (__DEV__) {
-    routes.children.unshift({
-        path: '/error',
-        action: require('./error').default,
-    });
+        );
+    }
+
 }
 
-export default routes;
+
+export default App;
