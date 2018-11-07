bd970384686252657a8a118307b926c2e9132822
PC\Administrator <15239928381@163.com>
2018-11-07 22:58:27 +0800

    no message

--- React/App/src/index.js
+++ React/App/src/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './layouts/Home/App';
-import * as serviceWorker from './services/Common/serviceWorker';
+import * as serviceWorker from './services/serviceWorker';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { Router, Route } from 'react-router';
+
+import AppLeft from './scenes/_common/containers/AppLeft/index'
+import AppBody from './scenes/_common/containers/APPBody/index'
+import AppHead from "./scenes/_common/containers/AppHead/index";
+
+import Home from "./scenes/Home/index";
+import News from "./scenes/News/index";
+
+import { Row, Col } from 'antd';
+
+require("./scenes/_common/css/global.scss");
+
+// package.json 默认使用的是 node_modules 下的 react-scripts/config/webpack.config.dev.js
+
+
+
+const App = (
+
+    <BrowserRouter >
+      <div id="App">
+            <AppHead />
+            <div id="AppMain">
+                <Row>
+                    <Col span={4}><AppLeft /></Col>
+                    <Col span={20}>
+                        <AppBody>
+                            <Route path="/" component={Home}>
+                                <Route path="home" component={Home} />
+                             	<Route path="news" component={News} />
+                            </Route>
+                        </AppBody>
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    </BrowserRouter>
+
+);
+
+
 
 // 项目的入口文件
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById('Root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- React/App/src/scenes/Home/index.js
+++ React/App/src/scenes/Home/index.js
@@ -1,74 +1,28 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import React, {Component} from 'react';
+import {findDOMNode} from 'react-dom';
 
-/* eslint-disable global-require */
+import { Row, Col } from 'antd';
 
-// The top-level (parent) route
-const routes = {
-    path: '',
+class Home extends Component{
 
-    // Keep in mind, routes are evaluated in order
-    children: [
-        {
-            path: '',
-            load: () => import(/* webpackChunkName: 'home' */ './home'),
-        },
-        {
-            path: '/contact',
-            load: () => import(/* webpackChunkName: 'contact' */ './contact'),
-        },
-        {
-            path: '/login',
-            load: () => import(/* webpackChunkName: 'login' */ './login'),
-        },
-        {
-            path: '/register',
-            load: () => import(/* webpackChunkName: 'register' */ './register'),
-        },
-        {
-            path: '/about',
-            load: () => import(/* webpackChunkName: 'about' */ './about'),
-        },
-        {
-            path: '/privacy',
-            load: () => import(/* webpackChunkName: 'privacy' */ './privacy'),
-        },
-        {
-            path: '/admin',
-            load: () => import(/* webpackChunkName: 'admin' */ './admin'),
-        },
+    constructor(props) {
 
-        // Wildcard routes, e.g. { path: '(.*)', ... } (must go last)
-        {
-            path: '(.*)',
-            load: () => import(/* webpackChunkName: 'not-found' */ './not-found'),
-        },
-    ],
+        super(props);
+        this.state = {date:new Date()};
+    }
 
-    async action({ next }) {
-        // Execute each child route until one of them return the result
-        const route = await next();
+    render(){
 
-        // Provide default values for title, description etc.
-        route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
-        route.description = route.description || '';
+        return (
 
-        return route;
-    },
-};
+             <div id="Route-to-Home">
+                <p>Home</p>
+            </div>
 
-// The error page is available by permanent url for development mode
-if (__DEV__) {
-    routes.children.unshift({
-        path: '/error',
-        action: require('./error').default,
-    });
+        );
+    }
+
 }
 
-export default routes;
+
+export default Home;
