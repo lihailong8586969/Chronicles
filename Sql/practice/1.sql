c2315225cea4656cf6668f443d2ffcf7ef8a637c
PC\Administrator <15239928381@163.com>
2018-11-15 23:58:21 +0800

    no message

--- Sql/practice/1.sql
+++ Sql/practice/1.sql
@@ -57,3 +57,15 @@ From c
 
 Where c.promo_cnt = (select max(promo_cnt) from c)
 
+# 7
+select * from table_name limit 3,1;                # 跳过前3条数据，从数据库中第4条开始查询，取一条数据，即第4条数据
+select * from table_name limit 3 offset 1;      # 从数据库中的第2条数据开始查询3条数据，即第2条到第4条
+
+# 8
+select department_ id ,AVG(salary) from employees group by department_ id ;
+
+# 9
+select * from orders as e 
+where (select count(distinct(e1.orderNo)) from orders as e1
+ where  e1.city = e.city and e1.orderNo > e.orderNo) < 3 ;
+# where (select count(distinct(e1.orderNo)) ???
