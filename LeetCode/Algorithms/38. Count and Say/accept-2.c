00edb79366fc23735e0de5c5c74649ef57547f45
PC\Administrator <15239928381@163.com>
2018-11-23 21:10:44 +0800

    no message

--- LeetCode/Algorithms/38. Count and Say/accept-2.c
+++ LeetCode/Algorithms/38. Count and Say/accept-2.c
@@ -1,4 +1,6 @@
-static char map[31][9999]={"0","1","11","21","1211"};
+// 经过测试发现 n=30 时, 序列的长度为 4462 
+// 因此，循环长度减小即可优化速度了
+static char map[31][4470]={"0","1","11","21","1211"};
 
 int calculate_count(char *s, int start, char target){
     
@@ -24,7 +26,7 @@ void auto_generate(){
     for(int i=5;i<=30;++i){
         
         int k=0, count=0;
-        for(int j=0;map[i-1][j]!='\0'&&k<=9997; j+=count,k+=1){
+        for(int j=0;map[i-1][j]!='\0'&&k<=4468; j+=count,k+=1){
             
             count = calculate_count(map[i-1], j , map[i-1][j]);
             map[i][k]=48+count;
