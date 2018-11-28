4a3531dd811f47b4eb41ff2d894de0b32e3e3fab
PC\Administrator <15239928381@163.com>
2018-11-28 21:15:18 +0800

    no message

---
+++ LeetCode/Algorithms/507. Perfect Number/Accepted-3.c
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+       
+        if(num<=1){
+            
+            return false;
+        }
+        
+        // 奇数一定不是
+        if(num%2!=0){
+            
+            return false;
+        }
+        
+        int i , sum=1;
+        double max=Math.sqrt(num);
+        for(i=2; sum<num && i<=max ; i=i*2){
+            
+            if(num%i==0){
+                
+                sum=sum+i+(num/i);
+            }
+        }
+        
+        if(sum != num){
+            
+            return false;
+        }
+        
+        return true;
+    }
+}
