400863f994e13196265d419f18ee3bacef505996
PC\Administrator <15239928381@163.com>
2018-12-09 11:04:51 +0800

    no message

---
+++ Java/JavaSE/learning/Integer.bitCount( ).java
@@ -0,0 +1,13 @@
+
+// https://blog.csdn.net/u011497638/article/details/77947324
+// 对 Integer.bitCount() 的理解
+
+public static int bitCount(int i) {
+        i = i - ((i >>> 1) & 0x55555555);
+        i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+        i = (i + (i >>> 4)) & 0x0f0f0f0f;
+        i = i + (i >>> 8);
+        i = i + (i >>> 16);
+        return i & 0x3f;
+}
+
---
+++ Php/CURL/curl_baidu_img.php
@@ -0,0 +1,10 @@
+<?php  
+
+// https://onestopweb.iteye.com/blog/2399975
+
+$brand = 'PHP';  
+   
+$url = "http://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=index&fr=&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word={$brand}&oq={$brand}&rsp=-1";  
+$content = file_get_contents($url);  
+preg_match_all('/objURL":"(.*?)",/',$content,$match);  
+print_r($match);  
