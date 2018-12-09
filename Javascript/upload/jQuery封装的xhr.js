7aad220752ed72455e29c0ba198e7285605bb1a8
PC\Administrator <15239928381@163.com>
2018-12-09 13:55:09 +0800

    no message

---
+++ Javascript/upload/jQuery封装的xhr.js
@@ -0,0 +1,42 @@
+
+
+// jQuery封装了xhr的实现, 也可以使用jQuery的ajax获得上传进度，示例代码：
+
+var formData = new FormData(); 
+formData.append("file", document.getElementById('file').files[0]); 
+formData.append("token", token_value); 
+
+$.ajax({ 
+    url: "/uploadurl", 
+    type: "POST", 
+    data: formData, 
+    processData: false, // 不要对data参数进行序列化处理，默认为true
+    contentType: false, // 不要设置Content-Type请求头，因为文件数据是以 multipart/form-data 来编码
+    xhr: function(){
+        myXhr = $.ajaxSettings.xhr();
+        if(myXhr.upload){
+          myXhr.upload.addEventListener('progress',function(e) {
+            if (e.lengthComputable) {
+              var percent = Math.floor(e.loaded/e.total*100);
+              if(percent <= 100) {
+                $("#J_progress_bar").progress('set progress', percent);
+                $("#J_progress_label").html('已上传：'+percent+'%');
+              }
+              if(percent >= 100) {
+                $("#J_progress_label").html('文件上传完毕，请等待...');
+                $("#J_progress_label").addClass('success');
+              }
+            }
+          }, false);
+        }
+        return myXhr;
+    },
+    success: function(res){ 
+        // 请求成功
+    },
+    error: function(res) {
+        // 请求失败
+        console.log(res);
+    }
+}); 
+ 
---
+++ Javascript/upload/vue-resource.js
@@ -0,0 +1,39 @@
+var formData = new FormData();
+formData.append('token', token_value);  // csrf token
+formData.append("works", document.getElementById('file').files[0]); // file
+var url = $("#R_batch_upload_url").val();
+
+vm.$http.post(url, formData, {
+  progress: (e) => {
+    if (e.lengthComputable) {
+      var percent = Math.floor(e.loaded/e.total*100);
+      if(percent <= 100) {
+        $("#J_progress_bar").progress('set progress', percent);
+        $("#J_progress_label").html('已上传：'+percent+'%');
+      }
+      if(percent >= 100) {
+        $("#J_progress_label").html('文件上传完毕，提交表单中，请等待...');
+        $("#J_progress_label").addClass('success');
+      }
+    }
+  }
+})
+.then((res) => {
+  if(res.ok && res.status === 200) {
+    window.location.href = window.location.href;
+  }
+}, (res) => {
+  if(res.status === 400) {
+      $("#J_progress_label").html('文件格式错误，请修改后重试');
+      $("#J_progress_label").addClass('warning');
+      console.log(res);
+      vm.errMsg.show = true;
+      vm.errMsg.msg = res.body.msg;
+      vm.canSend = true;
+      // TODO hide the loader dimmer
+      $("#J_upload_batch").dimmer("hide");
+    } else {
+      $("#J_progress_label").html(res.statusText);
+      $("#J_progress_label").addClass('warning');
+    }
+});
