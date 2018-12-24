147d40171c4965f3b5db31ddc7f0ae2d9074ab86
Lvsi-China <15239928381@163.com>
2018-12-24 22:24:12 +0800

    no message

---
+++ C#/learning/Threads/创建线程.cs
@@ -0,0 +1,30 @@
+// https://www.cnblogs.com/gangle/p/9285094.html
+
+// 方法一：使用Thread类
+public static void Main(string[] args)
+{
+     ThreadStart threadStart = new ThreadStart(Calculate);//通过ThreadStart委托告诉子线程执行什么方法　　
+     Thread thread = new Thread(threadStart);
+     thread.Start();//启动新线程
+}
+
+public static void Calculate()
+{
+     Console.Write("执行成功");
+     Console.ReadKey();
+}
+
+
+// 方法三：使用ThreadPool.QueueworkItem
+
+WaitCallback w = new WaitCallback(Calculate);
+//下面启动四个线程,计算四个直径下的圆周长
+ThreadPool.QueueUserWorkItem(w, 1.0);
+ThreadPool.QueueUserWorkItem(w, 2.0);
+ThreadPool.QueueUserWorkItem(w, 3.0);
+ThreadPool.QueueUserWorkItem(w, 4.0);
+
+public static void Calculate(double Diameter)
+{
+   return Diameter * Math.PI;
+}
